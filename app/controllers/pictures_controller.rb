0c06792a4bd2228e3122cf31fed1322c37101960
Yusuke Dev <ysk.kns.dev@gmail.com>
2017-02-05 21:22:52 +0900

    added edit/update actions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -12,6 +12,16 @@ class PicturesController < ApplicationController
     redirect_to pictures_path
   end
 
+  def edit
+    @picture = Picture.find(params[:id])
+  end
+
+  def update
+    @picture = Picture.find(params[:id])
+    @picture.update(pictures_params)
+    redirect_to pictures_path
+  end
+
   private
     def pictures_params
       params.require(:picture).permit(:image, :caption)
