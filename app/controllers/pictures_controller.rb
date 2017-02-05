d1960210263a34fb37dbc9874302b184b83e7de4
Yusuke Dev <ysk.kns.dev@gmail.com>
2017-02-05 15:23:42 +0900

    added new/create actions

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,18 @@
 class PicturesController < ApplicationController
   def index
   end
+
+  def new
+    @picture = Picture.new
+  end
+
+  def create
+    Picture.create(pictures_params)
+    redirect_to pictures_path
+  end
+
+  private
+    def pictures_params
+      params.require(:picture).permit(:image, :caption)
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  get 'pictures/index'
+  resources :pictures, only: [:index, :create, :new, :edit, :update, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
