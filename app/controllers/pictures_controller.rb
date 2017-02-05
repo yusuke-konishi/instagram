1d36434185e59a7b5d73812e60aedf9a26ddefe8
Yusuke Dev <ysk.kns.dev@gmail.com>
2017-02-05 23:01:50 +0900

    added flash message

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -9,7 +9,7 @@ class PicturesController < ApplicationController
 
   def create
     Picture.create(pictures_params)
-    redirect_to pictures_path
+    redirect_to pictures_path, notice: "写真を投稿しました！"
   end
 
   def edit
@@ -19,13 +19,13 @@ class PicturesController < ApplicationController
   def update
     @picture = Picture.find(params[:id])
     @picture.update(pictures_params)
-    redirect_to pictures_path
+    redirect_to pictures_path, notice: "投稿を更新しました！"
   end
 
   def destroy
     @picture = Picture.find(params[:id])
     @picture.destroy
-    redirect_to pictures_path
+    redirect_to pictures_path, notice: "投稿を削除しました！"
   end
 
   private
