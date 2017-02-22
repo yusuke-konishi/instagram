0c9e7de31680adfe105db2767c5225abc3b32763
Yusuke Dev <ysk.kns.dev@gmail.com>
2017-02-22 09:06:22 +0900

    modified sns login to ja, mypage to without password

--- app/models/user.rb
+++ app/models/user.rb
@@ -47,4 +47,13 @@ class User < ActiveRecord::Base
   def self.create_unique_string
     SecureRandom.uuid
   end
+
+  def update_with_password(params, *options)
+    if provider.blank?
+      super
+    else
+      params.delete :current_password
+      update_without_password(params, *options)
+    end
+  end
 end
